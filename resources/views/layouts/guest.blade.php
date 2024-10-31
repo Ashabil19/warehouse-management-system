4f796ca2d8e131a329cba4a5da77b4591d143883
ashabilsyauqi <ashabilsyauqi@gmail.com>
2024-10-31 17:03:05 +0700

    update sedikit di guest.blade

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,21 +17,19 @@
     <body class="font-sans text-gray-900 antialiased bg-gray-100">
         <div class="min-h-screen flex">
             <!-- Left Section -->
-            <div class="w-1/2 bg-cover bg-center relative hidden md:flex flex-col items-center justify-center p-10 text-white space-y-4" style="background-image: url('https://www.mintsoft.com/media/chzlxsn3/types_of_warehouse_blog.png?width=1200&height=630&v=1dab8149acd8d70'); background-size: cover; background-position: center;">
+            <div class="w-1/2 bg-cover bg-center relative hidden md:flex flex-col items-center justify-center p-10 text-white space-y-4" style="background-image: url('https://www.mintsoft.com/media/chzlxsn3/types_of_warehouse_blog.png?width=1200&height=630&v=1dab8149acd8d70');">
                 <!-- Semi-transparent overlay -->
                 <div class="absolute inset-0 bg-purple-800 opacity-75"></div>
             
                 <!-- Content inside the overlay -->
-                <div class="relative z-10">
+                <div class="relative z-10 text-center">
                     <div class="mb-5">
-                        {{-- <img src="/path/to/your/logo.png" alt="Logo" class="w-20"> --}}
-                        <img src="{{ asset('assets/taharica-logo.png') }}" alt="Logo" class="w-20">
+                        <img src="{{ asset('assets/taharica-logo.png') }}" alt="Logo" class="w-16 mb-6">
                     </div>
                     <h1 class="text-5xl font-bold">Welcome to GudangEasy</h1>
                     <p class="text-lg">Your trusted warehouse management solution.</p>
                 </div>
             </div>
-            
 
             <!-- Right Section (Login Form) -->
             <div class="flex flex-col justify-center items-center w-full md:w-1/2 bg-white p-8">

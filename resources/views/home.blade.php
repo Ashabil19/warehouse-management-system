15c2966c4e4f8f087b8777625e04803058255fab
ashabilsyauqi <ashabilsyauqi@gmail.com>
2025-01-17 09:43:09 +0700

    push testindo logo v6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,8 @@
         <div class="flex flex-col items-center justify-center h-screen text-center">  
             <h1 class="text-3xl font-semibold mb-4">Welcome Back!</h1>  
             <p class="text-lg text-gray-700 mb-6">You are already logged in.</p>  
+            <img src="{{ asset('images/testindo-logo.jpg') }}" alt="Testindo Logo" class="rounded-lg shadow-lg mb-6">  
+
         </div>  
     @endif  
 @endsection  

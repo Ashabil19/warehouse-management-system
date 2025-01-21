29d0d2db59cc75671463acd2a606317b61a36d0d
ashabilsyauqi <ashabilsyauqi@gmail.com>
2025-01-21 09:41:41 +0700

    perubahan koneksi ke vitte v.12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,21 @@
         
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+        @php  
+        $isProduction = app()->environment('production');  
+        $manifestPath = $isProduction ? '../public_html/build/manifest.json' : public_path('build/manifest.json');  
+    @endphp  
+      
+    @if ($isProduction && file_exists($manifestPath))  
+        @php  
+            $manifest = json_decode(file_get_contents($manifestPath), true);  
+        @endphp  
+        <link rel="stylesheet" href="{{ config('app.url') }}/build/{{ $manifest['resources/css/app.css']['file'] }}">  
+        <script type="module" src="{{ config('app.url') }}/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>  
+    @else  
+        @viteReactRefresh  
+        @vite(['resources/js/app.js', 'resources/css/app.css'])  
+    @endif          <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 
     </head>
     <body class="font-sans antialiased">
